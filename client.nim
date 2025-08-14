c359b9046523db17e1a252b0bd75855571ad21e7
Lothar Jöckel <lothar.joeckel@gmail.com>
2025-08-14 00:39:45 +0000

    templates for timing

--- client.nim
+++ client.nim
@@ -1,13 +1,29 @@
 import std/strformat
 import libs/nim_yottadb
 import std/strutils 
+import std/times
 
+const 
+  MAX = 100000
+  LOG = false
 
-const MAX=10
+template print(str: varargs[string]) =
+  if LOG:
+    for s in str: stdout.write(s)
+    stdout.writeLine("")
+
+template execute(title: string, body: untyped): untyped =
+  let time = cpuTime()
+  try:
+    body
+  except:
+    echo getCurrentExceptionMsg()
+  let diff = (cpuTime() - time) * 1000
+  echo title, ": finished in ", formatFloat(diff, ffDecimal, 0, '\0') ," ms."
+  echo ""
 
 func keysToString(global: string, subscript: seq[string]): string =
   result = global & "("
-
   for i, idx in subscript:
     try:
       let nmbr = parseInt(idx)
@@ -17,68 +33,44 @@ func keysToString(global: string, subscript: seq[string]): string =
 
     if i < subscript.len - 1:
       result.add(",")
-  
+
   result.add(")")
 
-
 proc writeData() =
-  echo "Write data"
   for i in 0..MAX:
-    try:
-      # Save in yottadb
-      ydb_set("^LJ", @["LAND", "ORT", $i], fmt"Hello Lothar Jöckel {i} aus der Schweiz")
-      ydb_set("^LJ", @["LAND", "ORT", $i, $i], fmt"Hello Lothar Jöckel {i} from Switzerland")
-    except YottaDbError:
-      echo getCurrentExceptionMsg()
+    ydb_set("^LJ", @["LAND", "ORT", $i], fmt"Hello Lothar Jöckel {i} aus der Schweiz")
+    ydb_set("^LJ", @["LAND", "ORT", $i, $i], fmt"Hello Lothar Jöckel {i} from Switzerland")
 
   ydb_set("^LJ", @["LAND", "STRASSE"], fmt"Gartenweg 4")
 
 proc readBack() =
-  echo "Read back"
   for i in 0..MAX:
-    try:
-      let result = ydb_get("^LJ", @["LAND", "ORT", $i])
-      assert result == fmt"Hello Lothar Jöckel {i} aus der Schweiz"
-    except YottaDbError:
-      echo getCurrentExceptionMsg()
-
+    let result = ydb_get("^LJ", @["LAND", "ORT", $i])
+    assert result == fmt"Hello Lothar Jöckel {i} aus der Schweiz"
 
 proc testData() =
-  echo "Test Data"
-  try:
-    assert ydb_data("^LJ", @["XXX"]) == 0 # There is neither a value nor a subtree, i.e., it is undefined.
-    assert ydb_data("^LJ", @["LAND"]) == 10 # There is no value, but there is a subtree.
-    assert ydb_data("^LJ", @["LAND", "ORT", "1"]) == 11 # There are both a value and a subtree.
-    assert ydb_data("^LJ", @["LAND", "STRASSE"]) == 1 # There is a value, but no subtree
+  assert ydb_data("^LJ", @["XXX"]) == 0 # There is neither a value nor a subtree, i.e., it is undefined.
+  assert ydb_data("^LJ", @["LAND"]) == 10 # There is no value, but there is a subtree.
+  assert ydb_data("^LJ", @["LAND", "ORT", "1"]) == 11 # There are both a value and a subtree.
+  assert ydb_data("^LJ", @["LAND", "STRASSE"]) == 1 # There is a value, but no subtree
 
-    doAssertRaises(YottaDbError): discard ydb_data("^LJ", @[""])
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
-
+  doAssertRaises(YottaDbError): discard ydb_data("^LJ", @[""])
 
 proc traverseNext(global: string, start_subscript: seq[string] = @[]) =
   var rc: int
   var subscript = start_subscript
-  try:
-    while true:
-      (rc, subscript) = ydb_node_next(global, subscript)
-      if rc != YDB_OK: break
-      echo keysToString(global, subscript)
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
-
+  while true:
+    (rc, subscript) = ydb_node_next(global, subscript)
+    if rc != YDB_OK: break
+    print keysToString(global, subscript)
 
 proc traversePrevious(global: string, start_subscript: seq[string] = @[]) =
   var rc: int
   var subscript = start_subscript
-  try:
-    while true:
-      (rc, subscript) = ydb_node_previous(global, subscript)
-      if rc != YDB_OK: break
-      echo keysToString(global, subscript)
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
-
+  while true:
+    (rc, subscript) = ydb_node_previous(global, subscript)
+    if rc != YDB_OK: break
+    print keysToString(global, subscript)
 
 proc nextSubscript() =
   let global = "^LL"
@@ -102,53 +94,33 @@ proc nextSubscript() =
   ydb_set("^LL", @["LAND", "NUTZUNG"])
   ydb_set("^LL", @["ORT"])
 
-  echo "Getting next subscript"
-  try:
-    var subscript = @["HAUS", "ELEKTRIK", ""]
-    var rc = 0
-    while(rc == YDB_OK):
-      rc = ydb_subscript_next(global, subscript)
-      echo "rc=", rc, "keys=", subscript
-  except YottaDbError:
-      echo getCurrentExceptionMsg()
+  var subscript = @["HAUS", "ELEKTRIK", ""]
+  var rc = 0
+  while(rc == YDB_OK):
+    rc = ydb_subscript_next(global, subscript)
+    print("rc=" & $rc & "keys=" & subscript)
 
   echo "Getting previous subscript"
-  try:
-    var subscript = @["HAUS", "HEIZUNG"]
-    var rc = 0
-    while(rc == YDB_OK):
-      rc = ydb_subscript_previous(global, subscript)
-      echo "rc=", rc, "keys=", subscript
-  except YottaDbError:
-      echo getCurrentExceptionMsg()
-
+  subscript = @["HAUS", "HEIZUNG"]
+  rc = 0
+  while(rc == YDB_OK):
+    rc = ydb_subscript_previous(global, subscript)
+    print("rc=" & $rc & "keys=" & subscript)
 
 proc deleteTree() =
-  echo "Delete Tree"
-  try:
-    var rc = ydb_delete_node("^LJ", @["LAND", "STRASSE"])
-    for i in 0..MAX:
-      rc = ydb_delete_tree("^LJ", @["LAND", "ORT", $i, $i])
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
-
+  var rc = ydb_delete_node("^LJ", @["LAND", "STRASSE"])
+  for i in 0..MAX:
+    rc = ydb_delete_tree("^LJ", @["LAND", "ORT", $i, $i])
 
 proc deleteNode() =
-  echo "Delete Node"
-  try:
-      var rc = ydb_delete_node("^CNT", @["CHANNEL", "INPUT"])
-      var result = ydb_increment("^CNT", @["CHANNEL", "INPUT"], 1)
-      assert ydb_get("^CNT", @["CHANNEL", "INPUT"]) == "1"
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
+    var rc = ydb_delete_node("^CNT", @["CHANNEL", "INPUT"])
+    var result = ydb_increment("^CNT", @["CHANNEL", "INPUT"], 1)
+    assert ydb_get("^CNT", @["CHANNEL", "INPUT"]) == "1"
 
 proc deleteGlobalVar() =
-  echo "Delete Global var ^LJ"
-  echo ydbmsg(ydb_delete_node("^LJ", @[]))
-
+  discard ydb_delete_node("^LJ", @[])
 
 proc getSpecialVariables() =
-  echo "getSpecialVariables"
   let vars = ["$DEVICE", "$ECODE","$ESTACK", "$ETRAP", "$HOROLOG",
               "$IO", "$JOB", "$KEY", "$PRINCIPAL", "$QUIT", "$REFERENCE", "$STACK", "$STORAGE",
               "$SYSTEM", "$TLEVEL", "$TRESTART", "$X", "$Y", "$ZA", "$ZALLOCSTOR", "$ZAUDIT",
@@ -161,54 +133,41 @@ proc getSpecialVariables() =
               "$ZUSEDSTOR", "$ZUT", "$ZVERSION", "$ZYERROR", "$ZYINTRSIG", "$ZYRELEASE", 
               "$ZYSQLNULL"]
   for variable in vars:
-    try:
-      echo variable, "=", ydb_get(variable)
-    except:
-      echo "Error when getting variable ", variable, ": ", getCurrentExceptionMsg()
+    print variable, "=", ydb_get(variable)
 
-  try:
-    let s = ydb_get("$XXXX")
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
-
+  let s = ydb_get("$XXXX")
 
 proc setAndGetVariable() =
-  echo "setAndGetVariable"
   let vars = ["X"]
   for variable in vars:
-    try:
-      echo variable
-      ydb_set(variable, @[], "hello")
-      ydb_set("X", @["1"], "hello X(1)")
-      ydb_set("X", @["1","1"], "hello X(1,1)")
-      ydb_set("X", @["1","2"], "hello X(1,2)")
-      ydb_set("X", @["1","3"], "hello X(1,3)")
-      ydb_set("X", @["2"], "hello X(2)")
-      ydb_set("X", @["2","3"], "hello X(2,3)")
-      #echo variable, "=", ydb_set(variable, @[], value="Hello")
-    except:
-      echo "Error when setting variable ", variable, ": ", getCurrentExceptionMsg()
+    ydb_set(variable, @[], "hello")
+    ydb_set("X", @["1"], "hello X(1)")
+    ydb_set("X", @["1","1"], "hello X(1,1)")
+    ydb_set("X", @["1","2"], "hello X(1,2)")
+    ydb_set("X", @["1","3"], "hello X(1,3)")
+    ydb_set("X", @["2"], "hello X(2)")
+    ydb_set("X", @["2","3"], "hello X(2,3)")
 
-  try:
-    echo "X=", (ydb_get("X"))
-    echo "X(1)=", (ydb_get("X", @["1"]))
-    echo "X(2)=", (ydb_get("X", @["2"]))
-    echo "X(1,1)=", (ydb_get("X", @["1","1"]))
-    traverseNext("X")  
-  except YottaDbError:
-    echo getCurrentExceptionMsg()
+  print "X=", (ydb_get("X"))
+  print "X(1)=", (ydb_get("X", @["1"]))
+  print "X(2)=", (ydb_get("X", @["2"]))
+  print "X(1,1)=", (ydb_get("X", @["1","1"]))
+  traverseNext("X")  
 
+# -------------------------------------------------------------------
 
-writeData()
-readBack()
-testData()
-traverseNext("^LJ")
-deleteTree()
-deleteNode()
-deleteGlobalVar()
-nextSubscript()
-getSpecialVariables()
-setAndGetVariable()
-traverseNext("^LJ") # get all keys
-traverseNext("^LJ", @["LAND", "ORT", "5"]) # start at ^LJ("LAND","ORT","5") -> 10
-traversePrevious("^LJ", @["LAND", "ORT", "5"]) # start at ^LJ("LAND","ORT","5") -> 0
+execute "ALL TESTS":
+
+  execute "writeData":  writeData()
+  execute "readBack": readBack()
+  execute "testData": testData()
+  execute "traverseNext(^LJ)": traverseNext("^LJ")
+  execute "deleteTree": deleteTree()
+  execute "deleteNode": deleteNode()
+  execute "deleteGlobalVar": deleteGlobalVar()
+  execute "nextSubscript": nextSubscript()
+  execute "getSpecialVariables": getSpecialVariables()
+  execute "setAndGetVariable": setAndGetVariable()
+  execute "traverseNext(^LJ)": traverseNext("^LJ") # get all keys
+  execute "traveseNext(^LJ(LAND,ORT)": traverseNext("^LJ", @["LAND", "ORT", "5"]) # start at ^LJ("LAND","ORT","5") -> 10
+  execute "traversePrevious(^LJ(LAND, ORT, 5)": traversePrevious("^LJ", @["LAND", "ORT", "5"]) # start at ^LJ("LAND","ORT","5") -> 0
