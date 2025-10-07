c7c432fbc4d32204a62ea6bea2aab6431c593103
ljoeckel <lothar.joeckel@gmail.com>
2025-10-07 12:50:14 +0000

    test improved

--- src/tests/dsl_lock_test.nim
+++ src/tests/dsl_lock_test.nim
@@ -4,112 +4,87 @@ import yottadb
 import utils
 import malebolgia
 
+proc findLocks(ids: varargs[string]): bool =
+    result = true
+    for s in getLocksFromYottaDb():
+        var found: bool
+        for id in ids:
+            if s.find(id) != -1: 
+                found = true
+                break
+        result = result and found
+
+
 proc testSimpleLocks() =
     lock: { ^CNT("TEMPLATE_TEST") }
-    assert getLockCountFromYottaDb() == 1
+    assert findLocks("TEMPLATE_TEST")
 
-    block:
-        let id = "MYID"
-        lock: { ^CNT(id) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    var id = "MYID"
+    lock: { ^CNT(id) }
+    assert findLocks(id)
 
-    block:
-        let id = "MYID"
-        lock: { ^CNT(id), ^CNT("ABC"), ^XYZ(123) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID"
+    lock: { ^CNT(id), ^CNT("ABC"), ^XYZ(123) }
+    assert findLocks(id, "ABC", "123")
 
-    block:
-        let id = "MYID-1"
-        lock: { ^CNT(id), timeout=1}
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-1"
+    lock: { ^CNT(id), timeout=1}
+    assert findLocks(id)
 
-    block:
-        let id = "MYID-2"
-        lock: { ^CNT(id), ^CNT("ABC-2"), ^XYZ(123) , timeout=2}
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-2"
+    lock: { ^CNT(id), ^CNT("ABC-2"), ^XYZ(123) , timeout=2}
+    assert findLocks(id, "ABC-2", "123")
 
-    block:
-        let id = "MYID-3"
-        lock: {timeout="3", ^CNT(id), ^CNT("ABC-3"), ^XYZ(123) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-3"
+    lock: {timeout="3", ^CNT(id), ^CNT("ABC-3"), ^XYZ(123) }
+    assert findLocks(id, "ABC-3", "123")
 
-    block:
-        let id = "MYID-4"
-        lock: {timeout=4, ^CNT(id), ^CNT("ABC-4"), ^XYZ(123) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-4"
+    lock: {timeout=4, ^CNT(id), ^CNT("ABC-4"), ^XYZ(123) }
+    assert findLocks(id, "ABC-4", "123")
 
-    block:
-        let id = "MYID-0"
-        lock: {timeout=0, ^CNT(id), ^CNT("ABC-0"), ^XYZ(123) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-0"
+    lock: {timeout=0, ^CNT(id), ^CNT("ABC-0"), ^XYZ(123) }
+    assert findLocks(id, "ABC-0", "123")
 
-    block:
-        let id = "MYID--1"
-        lock: {timeout=-1, ^CNT(id), ^CNT("ABC-1"), ^XYZ(123) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID--1"
+    lock: {timeout=-1, ^CNT(id), ^CNT("ABC-1"), ^XYZ(123) }
+    assert findLocks(id, "ABC-1", "123")
 
-    block:
-        let id = "MYID-abc"
-        lock: {timeout="abc", ^CNT(id), ^CNT("ABC"), ^XYZ(123) }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-abc"
+    lock: {timeout="abc", ^CNT(id), ^CNT("ABC"), ^XYZ(123) }
+    assert findLocks(id, "ABC", "123")
 
-    block:
-        let id = "MYID-single"
-        lock: { ^CNT(id), timeout=1000000 }
-        var found: bool
-        for s in getLocksFromYottaDb():
-            if s.find(id) != -1: found = true
-        assert found
+    id = "MYID-single"
+    lock: { ^CNT(id), timeout=1000000 }
+    assert findLocks(id)
 
 proc testLocalVarLocks() =
     lock: { ^LL }
-    assert getLockCountFromYottaDb() == 1
+    assert findLocks("^LL")
 
-    let id = "0815"
+    var id = "0815"
     lock: {^ll(4711), ^xyz("ABC"), ^abc(id), timeout=774455}
+    assert findLocks(id, "ABC", "4711", "0815")
     assert getLockCountFromYottaDb() == 3
-
+    
     lock: { ^globalvar, localvar, ^anotherglobal }
+    assert findLocks("^globalvar", "localvar", "^anotherglobal")
     assert getLockCountFromYottaDb() == 3
 
     lock: { ^globalvar(4711), localvar(4711), ^anotherglobal(id) }
     assert getLockCountFromYottaDb() == 3
+    assert findLocks("4711", id)
 
     lock: { localvar }
-    assert getLockCountFromYottaDb() == 1
+    assert findLocks("localvar")
 
     lock: { localvar(4711) }
-    assert getLockCountFromYottaDb() == 1
+    assert findLocks("4711")
 
-    lock: { localvar, localvar2(4711), localvar("abc", 4711) }
+    lock: { localvar, localvar2(4711), localvar("def", 4713) }
     assert getLockCountFromYottaDb() == 3
+    assert findLocks("localvar", "4711", "4713", "def")
 
     lock: {} # release all locks
     assert getLockCountFromYottaDb() == 0
