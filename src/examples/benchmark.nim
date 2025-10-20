79dc697667d965912f982fa272a672f75e6aba1a
ljoeckel <lothar.joeckel@gmail.com>
2025-10-20 19:12:10 +0000

    #6, Enhanced benchmark test

--- src/examples/benchmark.nim
+++ src/examples/benchmark.nim
@@ -7,6 +7,7 @@ const MAX = 10_000_000
 kill:
     ^BENCHMARK
     ^BENCHMARK2
+    ^BENCHMARK3
 
 proc isEmpty(name: string): bool =
     var (rc, s) = nextnode: name
@@ -18,30 +19,9 @@ proc upcount() =
     for cnt in 0..<MAX:
         discard ydb_increment("^CNT", @["upcount"])
 
-proc upcount_dsl() =
-    # dsl
-    kill: ^CNT("upcount")
-    for cnt in 0..<MAX:
-        discard increment: ^CNT("upcount")
-
-proc upcount_indirect() =
-    var gblcnt = "^CNT(upcount)"
-    kill: @gblcnt
-    for cnt in 0..<MAX:
-        discard increment: @gblcnt
-
 proc setSimple() =
     for id in 0..<MAX:
         ydb_set("^BENCHMARK1",@[$id], $id)
-
-proc setSimple_dsl() =
-    for id in 0..<MAX:
-        setvar: ^BENCHMARK2(id)=id
-
-proc setSimple_indirect() =
-    for id in 0..<MAX:
-        let gbl = fmt"^BENCHMARK2({id})"
-        setvar: @gbl
 
 proc nextnode() =
     var cnt = 0
@@ -51,6 +31,23 @@ proc nextnode() =
         inc cnt
     assert cnt == MAX
 
+proc killnode() =
+    for id in 0..<MAX:
+        ydb_delete_node("^BENCHMARK1", @[$id])
+    var (rc, s) = nextnode: ^BENCHMARK1
+    assert isEmpty("^BENCHMARK1")
+
+
+proc upcount_dsl() =
+    # dsl
+    kill: ^CNT("upcount")
+    for cnt in 0..<MAX:
+        discard increment: ^CNT("upcount")
+
+proc setSimple_dsl() =
+    for id in 0..<MAX:
+        setvar: ^BENCHMARK2(id)=id
+
 proc nextnode_dsl() =
     var cnt = 0
     var (rc, subs) = nextnode: ^BENCHMARK2.seq
@@ -59,43 +56,58 @@ proc nextnode_dsl() =
         inc cnt
     assert cnt == MAX
 
-proc nextnode_indirect() =
-    var cnt = 0
-    var (rc, gbl) = nextnode: ^BENCHMARK2
-    while rc == YDB_OK:
-        (rc, gbl) = nextnode: @gbl
-        inc cnt
-    assert cnt == MAX
-
-proc killnode() =
-    for id in 0..<MAX:
-        ydb_delete_node("^BENCHMARK1", @[$id])
-    var (rc, s) = nextnode: ^BENCHMARK1
-    assert isEmpty("^BENCHMARK1")
-
 proc killnode_dsl() =
     for id in 0..<MAX:
         killnode: ^BENCHMARK2(id)
     assert isEmpty("^BENCHMARK2")
 
+
+proc upcount_indirect() =
+    var gblcnt = "^CNT(upcount)"
+    kill: @gblcnt
+    for cnt in 0..<MAX:
+        discard increment: @gblcnt
+
+proc setSimple_indirect() =
+    for id in 0..<MAX:
+        let gbl = fmt"^BENCHMARK3({id})"
+        setvar: @gbl = id
+
+proc nextnode_indirect() =
+    var cnt = 0
+    var (rc, gbl) = nextnode: ^BENCHMARK3
+    while rc == YDB_OK:
+        (rc, gbl) = nextnode: @gbl
+        inc cnt
+    assert cnt == MAX
+
 proc killnode_indirect() =
     for id in 0..<MAX:
-        let gbl = fmt"^BENCHMARK2({id})"
+        let gbl = fmt"^BENCHMARK3({id})"
         kill: @gbl
-    assert isEmpty("^BENCHMARK2")
+    assert isEmpty("^BENCHMARK3")
+
 
 when isMainModule:
-  suite "Benchmark Tests":
-    test "upcount": timed: upcount()
-    test "upcount dsl": timed: upcount_dsl()
-    test "upcount @": timed: upcount_indirect()
-    test "set simple": timed: setSimple()
-    test "set simple dsl": timed: setSimple_indirect()
-    test "set simple @": timed: setSimple_dsl()
-    test "nextnode": timed: nextnode()
-    test "nextnode dsl": timed: nextnode_dsl()
-    test "nextnode @": timed: nextnode_indirect()
-    test "killnode": timed: killnode()
-    test "killnode dsl": timed: killnode_dsl()
-    test "set simple dsl": timed: setSimple_indirect()
-    test "killnode @": timed: killnode_indirect()
+    timed:
+        suite "API":
+            test "upcount": timed: upcount()
+            test "set simple": timed: setSimple()
+            test "nextnode": timed: nextnode()
+            test "killnode": timed: killnode()
+        echo ""
+
+    timed:
+        suite "DSL":
+            test "upcount dsl": timed: upcount_dsl()
+            test "set simple dsl": timed: setSimple_dsl()
+            test "nextnode dsl": timed: nextnode_dsl()
+            test "killnode dsl": timed: killnode_dsl()
+        echo ""
+
+    timed:
+        suite "Indirection":
+            test "upcount @": timed: upcount_indirect()
+            test "set simple @": timed: setSimple_indirect()
+            test "nextnode @": timed: nextnode_indirect()
+            test "killnode @": timed: killnode_indirect()
