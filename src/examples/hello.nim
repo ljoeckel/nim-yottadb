c0cfd9742f49c9e7d7e654e3c2de93082780f265
ljoeckel <lothar.joeckel@gmail.com>
2026-01-09 08:31:41 +0000

    Updated doc and added hello.nim sample

---
+++ src/examples/hello.nim
@@ -0,0 +1,23 @@
+import std/strformat
+import yottadb
+
+proc main() =
+  setvar:
+    ^CUSTOMER(1, "Name")="John Doe"
+    ^CUSTOMER(1, "Email")="john-doe.@gmail.com"
+    ^CUSTOMER(2, "Name")="Jane Smith"
+    ^CUSTOMER(2, "Email")="jane.smith.@yahoo.com"
+
+  echo "Iterate over all customer id's"
+  for id in orderItr(^CUSTOMER):
+    let name = getvar  ^CUSTOMER(id, "Name")
+    let email = getvar  ^CUSTOMER(id, "Email")
+    echo fmt"Customer {id}: {name} <{email}>"
+
+  echo "Iterate over all nodes"
+  for node in queryItr ^CUSTOMER:
+    let value = getvar @node
+    echo fmt"{node}={value}"
+
+when isMainModule:
+  main()
